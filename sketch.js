17a552ebaa62c60f8384d38b87f8947436aefda1
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-05 21:19:30 -0500

    Bug found on load.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,44 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
 
+// Timer
+// https://youtu.be/MLtAMg9_Svw
 
 let timeleft = 10;
 
 let startTime = 0;
 let currentTime = 0;
 
-function setup() {
-    createCanvas(400, 400);
+function convertSeconds(s) {
+  let min = floor(s / 60);
+  let sec = s % 60;
+  return nf(min, 2) + ':' + nf(sec, 2);
 }
 
-function draw() {
-    background(220);
+
+function setup() {
+  noCanvas();
+  startTime = millis();
+
+  console.log(params);
+  if (params.minute) {
+    let min = params.minute;
+    timeleft = min * 60;
+  }
+
+  let timer = select('#timer');
+  timer.html(convertSeconds(timeleft - currentTime));
+
+  let interval = setInterval(timeIt, 1000);
+
+  function timeIt() {
+    currentTime = floor((millis() - startTime) / 1000);
+    timer.html(convertSeconds(timeleft - currentTime));
+    if (currentTime == timeleft) {
+
+      clearInterval(interval);
+      //counter = 0;
+    }
+  }
 }
