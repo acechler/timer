50a104d8709d505e960d60d002682eb2330f6faa
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-05 21:22:41 -0500

    Implement the timer.

--- sketch.js
+++ sketch.js
@@ -1,44 +1,39 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Timer
-// https://youtu.be/MLtAMg9_Svw
-
 let timeleft = 10;
-
 let startTime = 0;
 let currentTime = 0;
 
-function convertSeconds(s) {
-  let min = floor(s / 60);
-  let sec = s % 60;
-  return nf(min, 2) + ':' + nf(sec, 2);
-}
+// Assuming params is defined here or brought in from another script
+let params = { minute: 5 }; // Example parameter
 
+function convertSeconds(s) {
+    let min = Math.floor(s / 60);
+    let sec = s % 60;
+    return nf(min, 2) + ':' + nf(sec, 2);
+}
 
 function setup() {
-  noCanvas();
-  startTime = millis();
+    noCanvas();
+    startTime = millis();
 
-  console.log(params);
-  if (params.minute) {
-    let min = params.minute;
-    timeleft = min * 60;
-  }
-
-  let timer = select('#timer');
-  timer.html(convertSeconds(timeleft - currentTime));
-
-  let interval = setInterval(timeIt, 1000);
-
-  function timeIt() {
-    currentTime = floor((millis() - startTime) / 1000);
-    timer.html(convertSeconds(timeleft - currentTime));
-    if (currentTime == timeleft) {
-
-      clearInterval(interval);
-      //counter = 0;
+    if (params.minute) {
+        let min = params.minute;
+        timeleft = min * 60;
     }
-  }
+
+    let timer = select('#timer');
+    timer.html(convertSeconds(timeleft - currentTime));
+
+    let interval = setInterval(timeIt, 1000);
+
+    function timeIt() {
+        currentTime = Math.floor((millis() - startTime) / 1000);
+        timer.html(convertSeconds(timeleft - currentTime));
+        if (currentTime >= timeleft) {
+            clearInterval(interval);
+            // Actions to do after the countdown finishes
+        }
+    }
 }
+
+// Make sure to call setup to initialize everything
+setup();
